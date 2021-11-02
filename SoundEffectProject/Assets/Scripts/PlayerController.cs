8af88490a68cd6854a61077e96e22c2206bbdffd
Anika Boettcher <10945351@my.uvu.edu>
2021-11-02 14:55:55 -0600

    Added player animations

--- SoundEffectProject/Assets/Scripts/PlayerController.cs
+++ SoundEffectProject/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 
 {
     private Rigidbody playerRb;
+	private Animator playerAnim;
 
     public float jumpForce;
 
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+		playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -28,6 +30,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+			playerAnim.SetTrigger("Jump_trig");
         }
         
     }
