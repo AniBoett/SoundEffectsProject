70692e3874178f02d1ce0412b23f49f5a76cdef7
Anika Boettcher <10945351@my.uvu.edu>
2021-11-01 22:43:50 -0600

    Added Game Over! message

--- SoundEffectProject/Assets/Scripts/PlayerController.cs
+++ SoundEffectProject/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
 
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            //prevent the player from double jumping
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
@@ -31,7 +33,16 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
+    //trigger a game over when the player collides with an obstacle
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
